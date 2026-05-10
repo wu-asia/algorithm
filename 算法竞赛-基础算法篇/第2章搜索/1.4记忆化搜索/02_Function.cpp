7d0a809f59c40c04394977d28dc971b39591acf9
AsiaWu_a <wuasia2007@hotmail.com>
2026-05-10 16:13:03 +0800

    5/10

--- 算法竞赛-基础算法篇/第2章搜索/1.4记忆化搜索/02_Function.cpp
+++ 算法竞赛-基础算法篇/第2章搜索/1.4记忆化搜索/02_Function.cpp
@@ -8,25 +8,30 @@ const int N = 30;
 LL f[N][N][N];
 LL w(LL a, LL b, LL c)
 {
-	if(f[a][b][c] != -1) return f[a][b][c];
+	
 	if(a <= 0 || b <= 0 || c <= 0) return 1;
 	else if(a > 20 || b > 20 || c > 20) return w(20, 20, 20);
+	if(f[a][b][c]) return f[a][b][c];
 	else if(a < b && b < c)
-		{f[a][b][c] = f[a][b][c - 1] + f[a][b - 1][c - 1] - f[a][b - 1][c];
-				return f[a][b][c];}
+	{
+		f[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+		return f[a][b][c];
+	}
 	else
-		{f[a][b][c] = f[a - 1][b][c] + f[a - 1][b - 1][c] + f[a - 1][b][c - 1] - f[a - 1][b - 1][c - 1];
-				return f[a][b][c];}
+	{
+		f[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+		return f[a][b][c];
+	}
 }
 int main()
 {
-	int a, b, c;
+	LL a, b, c;
 	while(1)
 	{
-		for(int i = 0; i < N; i++)
-			for(int j = 0; j < N; j++)
-				for(int k = 0; k < N; k++)
-					f[i][j][k] = -1;
+	// 	for(int i = 0; i < N; i++)
+	// 		for(int j = 0; j < N; j++)
+	// 			for(int k = 0; k < N; k++)
+	// 				f[i][j][k] = -1;
 		cin >> a >> b >> c;
 		if(a == -1 && b == -1 && c == -1)
 			break;
