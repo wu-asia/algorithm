4b8d96bc92580c5203ca721a0632d2fb9be392f4
AsiaWu_a <wuasia2007@hotmail.com>
2026-08-15 11:24:31 +0900

    algorithm 8/15

--- 算法竞赛-基础算法篇/第1章基础算法/1.07二分算法/06_砍树复习2.cpp
+++ 算法竞赛-基础算法篇/第1章基础算法/1.07二分算法/06_砍树复习2.cpp
@@ -2,9 +2,20 @@
 
 using namespace std;
 
+typedef long long LL;
+
+LL n, a, b;
 
 int main()
 {
-
+	cin >> n >> a >> b;
+	for(int i = 1; i <= n; i++)
+		cin >> w[i];
+	LL left = 1, right = 5e5;
+	while(left < right)
+	{
+		LL mid = (left + right) / 2;
+		if(check(mid)) 
+	}
 	return 0;
 }
--- 算法竞赛-算法巩固篇/第1周/Day07/04_奶牛晒衣服.cpp
+++ 算法竞赛-算法巩固篇/第1周/Day07/04_奶牛晒衣服.cpp
@@ -3,13 +3,35 @@
 using namespace std;
 
 int a, b, n;
-//
-int main()
+const int N = 5e5 + 10;
+int w[N];
+
+bool check(int x)
 {
-	cin >> n >> a >> n;
+	int cnt = 0;
 	for(int i = 1; i <= n; i++)
 	{
 		
+		if(w[i] <= a * x)
+			continue;
+		int d = w[i] - a * x;
+		cnt += ((d / b) + (d % b == 0 ? 0 : 1));
 	}
+	return (cnt <= x);
+}
+int main()
+{
+	cin >> n >> a >> b;
+	for(int i = 1; i <= n; i++)
+		cin >> w[i];
+
+	int left = 1, right = 5e5;
+	while(left < right)
+	{
+		int mid = (left + right) / 2;
+		if(check(mid)) right = mid;
+		else left = mid + 1;
+	}
+	cout << left << endl;
 	return 0;
 }
