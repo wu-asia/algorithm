f1275460888e73094e7aee7bc92c3ea933f2fe5c
AsiaWu_a <wuasia2007@hotmail.com>
2026-09-14 15:38:07 +0800

    algorithm 9/14

---
+++ 算法竞赛-基础算法篇/第5章图论基础/4单源最短路径/06_spfa算法判断负环复习.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<queue>
+#include<utility>
+#include<vector>
+#include<cstring>
+
+using namespace std;
+const int N = 2e3 + 10;
+int n, m;
+typedef pair<int, int> PII;
+vector<PII> edges[N];
+int dist[N];
+bool st[N];
+int cnt[N];
+
+bool spfa()
+{
+	memset(dist, 0x3f, sizeof dist);
+	memset(st, 0, sizeof st);
+	memset(cnt, 0, sizeof cnt);
+	queue<int> q;
+	q.push(1);
+	st[1] = true;
+	while(q.size())
+	{
+		int u = q.front();
+		q.pop();
+		st[u] = false;
+		for(auto& e : edges[u])
+		{
+			int v = e.first, w = e.second;
+			if(dist[u] + w < dist[v])
+			{
+				dist[v] = dist[u] + w;
+				cnt[v] = cnt[u] + 1;
+				if(cnt[v] >= n) return true;
+			}
+			if(!st[v])
+			{
+				q.push(v);
+				st[v] = true;
+			}
+		}
+	}
+	return false;
+}
+
+int main()
+{
+	int T;
+	cin >> T;
+	while(T--)
+	{
+		cin >> n >> m;
+		for(int i = 1; i <= n; i++)
+			edges[i].clear();
+		for(int i = 1; i <= m; i++)
+		{
+			int u, v, w;
+			cin >> u >> v >> w;
+			edges[u].push_back({v, w});
+			if(w >= 0)
+				edges[v].push_back({u, w});
+		}
+		if(spfa()) cout << "YES" << endl;
+		else cout << "NO" << endl;
+	}
+	return 0;
+}
